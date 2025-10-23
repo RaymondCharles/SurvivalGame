07a98ed045599c4b45e8dfe1c1a4540e5fdbce18
NITHIN-20053 <rnithin2105@gmail.com>
2025-10-23 15:44:06 +0100

    Main Screen

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public Image img;
+    public Sprite default_img;
+    public Sprite pressed_img;
+    public AudioClip compressed;
+    public AudioClip uncompressed;
+    public AudioSource source;
+
+   
+    public string sceneToLoad;
+    public float loadDelay = 1f;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        img.sprite = pressed_img;
+        source.PlayOneShot(compressed);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        img.sprite = default_img;
+        source.PlayOneShot(uncompressed);
+        StartCoroutine(LoadSceneWithDelay());
+    }
+
+    private IEnumerator LoadSceneWithDelay()
+    {
+        yield return new WaitForSeconds(loadDelay);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
