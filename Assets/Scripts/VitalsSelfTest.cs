1f3afefad66c2dd1caf1dc594e0439e17ce595cb
NITHIN-20053 <rnithin2105@gmail.com>
2025-10-24 09:26:15 +0100

    HP UI Fix

--- Assets/Scripts/VitalsSelfTest.cs
+++ Assets/Scripts/VitalsSelfTest.cs
@@ -5,20 +5,20 @@ public class VitalsSelfTest : MonoBehaviour
 {
     public PlayerVitals vitals;
 
-    IEnumerator Start()
-    {
-        if (!vitals) vitals = FindObjectOfType<PlayerVitals>();
-        yield return new WaitForSeconds(0.5f);
+    //IEnumerator Start()
+    //{
+    //    if (!vitals) vitals = FindObjectOfType<PlayerVitals>();
+    //    yield return new WaitForSeconds(0.5f);
 
-        Debug.Log("[VitalsSelfTest] Damage 25");
-        vitals.Damage(25f);   // HP should drop
+    //    Debug.Log("[VitalsSelfTest] Damage 25");
+    //    vitals.Damage(25f);   // HP should drop
 
-        yield return new WaitForSeconds(0.8f);
-        Debug.Log("[VitalsSelfTest] Heal 10 via food");
-        vitals.Eat(0f, 10f);  // HP should rise a bit
+    //    yield return new WaitForSeconds(0.8f);
+    //    Debug.Log("[VitalsSelfTest] Heal 10 via food");
+    //    vitals.Eat(0f, 10f);  // HP should rise a bit
 
-        yield return new WaitForSeconds(0.8f);
-        Debug.Log("[VitalsSelfTest] Eat 30 hunger");
-        vitals.Eat(30f);      // Hunger should go up
-    }
+    //    yield return new WaitForSeconds(0.8f);
+    //    Debug.Log("[VitalsSelfTest] Eat 30 hunger");
+    //    vitals.Eat(30f);      // Hunger should go up
+    //}
 }
